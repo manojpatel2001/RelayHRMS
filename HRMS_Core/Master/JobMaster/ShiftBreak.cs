f8400e7d26cdf1762a7a7a1a364bbeee965ac21b
talkwithbrijesh <talkwithbrijesh@gmail.com>
2025-04-02 16:12:50 +0530

    table_migration for Shiftmaster_and_shoftbreaks

---
+++ HRMS_Core/Master/JobMaster/ShiftBreak.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMS_Core.Master.JobMaster
+{
+    [Table("ShiftBreak")]
+    public class ShiftBreak
+    {
+        [Key]
+        public int BreakID { get; set; }
+
+        public int ShiftID { get; set; }
+
+        [StringLength(50)]
+        public required string BreakName { get; set; }
+
+        public TimeSpan? StartTime { get; set; }
+
+        public TimeSpan? EndTime { get; set; }
+
+        public TimeSpan? Duration { get; set; }
+
+        public bool DeductBreak { get; set; } = false;
+
+        public bool DeductHour { get; set; } = false;
+
+        // Navigation property
+        [ForeignKey("ShiftID")]
+        public virtual ShiftMaster ShiftMaster { get; set; }
+    }
+
+}
---
+++ HRMS_Core/Master/JobMaster/ShiftMaster.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMS_Core.Master.JobMaster
+{
+    [Table("ShiftMaster")]
+    public class ShiftMaster
+    {
+        [Key]
+        public int ShiftID { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public required string ShiftName { get; set; }
+
+        [Required]
+        public TimeSpan StartTime { get; set; }
+
+        [Required]
+        public TimeSpan EndTime { get; set; }
+
+        public TimeSpan? Duration { get; set; }
+
+        public bool IsHalfDay { get; set; } = false;
+
+        public bool AutoShiftChange { get; set; } = false;
+
+        public bool IsActive { get; set; } = true;
+
+        public bool IsTrainingShift { get; set; } = false;
+
+        public bool IsSplitShift { get; set; } = false;
+
+        // Navigation property
+        public virtual ICollection<ShiftBreak> ShiftBreaks { get; set; }
+    }
+
+}
