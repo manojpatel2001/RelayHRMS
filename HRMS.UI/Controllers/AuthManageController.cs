95d9d2e4c55c5de01cd7c8e6a9e47a496fe131e4
talkwithbrijesh <talkwithbrijesh@gmail.com>
2025-04-09 21:46:03 +0530

    Layout updates

---
+++ HRMS.UI/Controllers/AuthManageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HRMS.UI.Controllers
+{
+    public class AuthManageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
