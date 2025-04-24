d92c387ffaf7afce94112f56f6242e9d66ef5aa8
RameshDeveloper001 <chramesh7526@gmail.com>
2025-04-25 04:30:53 +0530

    Create UI for Create employee ,Update employee, Create or update persional details

---
+++ HRMS.UI/Controllers/PartialViewController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HRMS.UI.Controllers
+{
+    public class PartialViewController : Controller
+    {
+        public IActionResult EmployeeInfo()
+        {
+            return PartialView("_EmployeeInfo");
+        }
+
+
+        public IActionResult Contact()
+        {
+            return PartialView("_Contact");
+        }
+
+        public IActionResult EmergencyContact()
+        {
+            return PartialView("_EmergencyContact");
+        }
+
+        public IActionResult Reporting()
+        {
+            return PartialView("_Reporting");
+        }
+
+        public IActionResult Attachments()
+        {
+            return PartialView("_Attachments");
+        }
+
+        public IActionResult Allowance()
+        {
+            return PartialView("_Allowance");
+        }
+
+        public IActionResult Salary()
+        {
+            return PartialView("_Salary");
+        }
+    }
+}
