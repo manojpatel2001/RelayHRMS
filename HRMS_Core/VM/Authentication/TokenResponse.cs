1e8e579bb6aaab835c8274bd9532fbc8a48b0362
RameshDeveloper001 <chramesh7526@gmail.com>
2025-06-01 01:58:43 +0530

    Create Login and Logout Employee

---
+++ HRMS_Core/VM/Authentication/TokenResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMS_Core.VM.Authentication
+{
+    
+        public class TokenResponse
+        {
+            public string? Token { get; set; }
+            public string? Email { get; set; }
+            public string? Id { get; set; }
+        }
+}
