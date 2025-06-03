5d3e456d54c8d14853e5f6aa24a7f42e128fe1c1
Nikita Suhas <nikuballal19@gmail.com>
2025-06-03 16:23:51 +0530

    Bulk Increment

---
+++ HRMS.UI/Controllers/BulkIncrementController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HRMS.UI.Controllers
+{
+    public class BulkIncrementController : Controller
+    {
+        public IActionResult BulkIncrement()
+        {
+            return View();
+        }
+    }
+}
