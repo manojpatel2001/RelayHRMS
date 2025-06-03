a6c8ebd63eb4f7d65f9c6e3e6d03272f961d0b05
Nikita Suhas <nikuballal19@gmail.com>
2025-06-03 15:11:37 +0530

    Employeeincreemntapplication,gradewiseallowance,employeeincrement

---
+++ HRMS.UI/Controllers/EmployeeIncrementController.cs
@@ -0,0 +1,17 @@
+﻿  using Microsoft.AspNetCore.Mvc;
+
+namespace HRMS.UI.Controllers
+{
+    public class EmployeeIncrementController : Controller
+    {
+        public IActionResult EmployeeIncrementApplication()
+        {
+            return View();
+        }
+
+        public IActionResult AddEmployeeIncrement()
+        {
+            return View();
+        }
+    }
+}
