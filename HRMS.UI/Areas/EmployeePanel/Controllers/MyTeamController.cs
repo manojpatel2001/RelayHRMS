9da9dcbdb82b1d18715229650956caa67205d9a0
HP <HP@DESKTOP-2VPE4JH>
2025-06-18 22:37:14 +0530

    create MY Team controller Member Shift menu design Shiftchnage view and AddShiftChanges view

---
+++ HRMS.UI/Areas/EmployeePanel/Controllers/MyTeamController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HRMS.UI.Areas.EmployeePanel.Controllers
+{
+    [Area("EmployeePanel")]
+    public class MyTeamController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public MyTeamController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            ViewBag.BaseUrl = _configuration["BaseUrlSettings:baseUrl"];
+
+            return View();
+        }
+        public IActionResult ShiftChanges()
+        {
+            ViewBag.BaseUrl = _configuration["BaseUrlSettings:baseUrl"];
+
+            return View();
+        }
+
+        public IActionResult AddShiftChanges()
+        {
+            ViewBag.BaseUrl = _configuration["BaseUrlSettings:baseUrl"];
+
+            return View();
+        }
+    }
+}
