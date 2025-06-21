1313c0aad22a74add69192ac36c8eab6e936dd04
HP <HP@DESKTOP-2VPE4JH>
2025-06-21 21:24:01 +0530

    2 leave Reports All Reports complete

--- HRMS.UI/Areas/EmployeePanel/Controllers/MyReportsController.cs
+++ HRMS.UI/Areas/EmployeePanel/Controllers/MyReportsController.cs
@@ -69,5 +69,61 @@ namespace HRMS.UI.Areas.EmployeePanel.Controllers
 
             return View();
         }
+        public IActionResult LeaveApproval()
+        {
+            ViewBag.BaseUrl = _configuration["UIBaseUrlSettings:baseUrl"];
+
+            return View();
+        }
+
+        public IActionResult LeaveBalance()
+        {
+            ViewBag.BaseUrl = _configuration["UIBaseUrlSettings:baseUrl"];
+
+            return View();
+        }
+        public IActionResult YearlyLeave()
+        {
+            ViewBag.BaseUrl = _configuration["UIBaseUrlSettings:baseUrl"];
+
+            return View();
+        }
+
+        public IActionResult Getpass()
+        {
+            ViewBag.BaseUrl = _configuration["UIBaseUrlSettings:baseUrl"];
+
+            return View();
+        }
+        public IActionResult Compoffleaveadjustment()
+        {
+            ViewBag.BaseUrl = _configuration["UIBaseUrlSettings:baseUrl"];
+
+            return View();
+        }
+        public IActionResult Compoffavailbalance()
+        {
+            ViewBag.BaseUrl = _configuration["UIBaseUrlSettings:baseUrl"];
+
+            return View();
+        }
+        public IActionResult LeaveCard()
+        {
+            ViewBag.BaseUrl = _configuration["UIBaseUrlSettings:baseUrl"];
+
+            return View();
+        }
+        public IActionResult LeaveEncashSlip()
+        {
+            ViewBag.BaseUrl = _configuration["UIBaseUrlSettings:baseUrl"];
+
+            return View();
+        }
+        public IActionResult leaveapplicationreport()
+        {
+            ViewBag.BaseUrl = _configuration["UIBaseUrlSettings:baseUrl"];
+
+            return View();
+        }
     }
 }
