398b48b5e5cea024661295f128b8492c39258ca2
HP <HP@DESKTOP-2VPE4JH>
2025-06-24 00:47:21 +0530

    Desigen EmployeeandTeam view

--- HRMS.UI/Areas/EmployeePanel/Controllers/HomeController.cs
+++ HRMS.UI/Areas/EmployeePanel/Controllers/HomeController.cs
@@ -17,6 +17,26 @@ namespace HRMS.UI.Areas.EmployeePanel.Controllers
             ViewBag.BaseUrl = _configuration["UIBaseUrlSettings:baseUrl"];
             return View();
         }
+        public IActionResult EmployeeandTeam()
+        {
+            ViewBag.BaseUrl = _configuration["UIBaseUrlSettings:baseUrl"];
+            return View();
+        }
+        public IActionResult Attendanceandleave()
+        {
+            ViewBag.BaseUrl = _configuration["UIBaseUrlSettings:baseUrl"];
+            return View();
+        }
+        public IActionResult Salary()
+        {
+            ViewBag.BaseUrl = _configuration["UIBaseUrlSettings:baseUrl"];
+            return View();
+        }
+        public IActionResult Reports()
+        {
+            ViewBag.BaseUrl = _configuration["UIBaseUrlSettings:baseUrl"];
+            return View();
+        }
     }
 
 }
