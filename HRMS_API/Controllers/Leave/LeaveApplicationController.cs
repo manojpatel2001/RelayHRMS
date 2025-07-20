8ec16b8b1e11f57e6a19733ded43751d0716a781
Rita Girase <ritagirase9@gmial.com>
2025-07-20 15:58:35 +0530

    Add Leave Balance Table in Add Leave

--- HRMS_API/Controllers/Leave/LeaveApplicationController.cs
+++ HRMS_API/Controllers/Leave/LeaveApplicationController.cs
@@ -235,5 +235,38 @@ namespace HRMS_API.Controllers.Leave
         }
 
 
+        [HttpPost("GetLeaveType")]
+        public async Task<APIResponse> GetLeaveType([FromBody] LeaveDetailsvm vm)
+        {
+            try
+            {
+                var data = await _unitOfWork.LeaveApplicationRepository.GetLeaveDetails(vm);
+
+                if (data == null || data.Count == 0)
+                {
+                    return new APIResponse
+                    {
+                        isSuccess = false,
+                        ResponseMessage = "No leave records found."
+                    };
+                }
+
+                return new APIResponse
+                {
+                    isSuccess = true,
+                    Data = data,
+                    ResponseMessage = "Leave records fetched successfully."
+                };
+            }
+            catch (Exception err)
+            {
+                return new APIResponse
+                {
+                    isSuccess = false,
+                    Data = err.Message,
+                    ResponseMessage = "Unable to retrieve leave records. Please try again later!"
+                };
+            }
+        }
     }
 }
