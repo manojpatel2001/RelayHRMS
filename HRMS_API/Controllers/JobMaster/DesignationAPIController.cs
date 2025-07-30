6514c0290b45e17e16e509725a7d6f6a5b1d17b5
HP <HP@DESKTOP-2VPE4JH>
2025-07-31 01:05:08 +0530

    dropdown sets

--- HRMS_API/Controllers/JobMaster/DesignationAPIController.cs
+++ HRMS_API/Controllers/JobMaster/DesignationAPIController.cs
@@ -202,5 +202,33 @@ namespace HRMS_API.Controllers.JobMaster
             }
         }
 
+        [HttpGet("GetAllDesignationByCompanyId/{companyId}")]
+        public async Task<APIResponse> GetAllDesignationByCompanyId(int companyId)
+        {
+            try
+            {
+                var data = await _unitOfWork.DesignationRepository.GetAllAsync(asp => asp.c == companyId && asp.IsEnabled == true && asp.IsDeleted == false);
+                if (data == null || !data.Any())
+
+                    return new APIResponse { isSuccess = false, ResponseMessage = "No records found." };
+
+                var newdata = data.Select(x => new
+                {
+                    DepartmentId = x.DepartmentId,
+                    DepartmentName = x.DepartmentName
+
+
+                });
+
+                return new APIResponse { isSuccess = true, Data = newdata, ResponseMessage = "Records fetched successfully." };
+
+
+            }
+            catch (Exception ex)
+            {
+                return new APIResponse { isSuccess = false, Data = ex.Message, ResponseMessage = "Unable to retrieve records. Please try again later." };
+            }
+        }
+
     }
 }
