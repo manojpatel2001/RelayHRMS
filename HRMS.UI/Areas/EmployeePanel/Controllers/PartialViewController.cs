3037513ec8eae40218322b9f792395f22f3e43a4
Rita Girase <ritagirase9@gmial.com>
2025-07-31 15:45:22 +0530

    show Save Button

--- HRMS.UI/Areas/EmployeePanel/Controllers/PartialViewController.cs
+++ HRMS.UI/Areas/EmployeePanel/Controllers/PartialViewController.cs
@@ -9,5 +9,54 @@ namespace HRMS.UI.Areas.EmployeePanel.Controllers
         {
             return PartialView("_PersonalInfo");
         }
+
+        public IActionResult Contact()
+        {
+            return PartialView("_Contact");
+        }
+        public IActionResult EmergencyContact()
+        {
+            return PartialView("_EmergencyContact");
+        }
+        public IActionResult Dependents()
+        {
+            return PartialView("_Dependents");
+        }
+        public IActionResult Immigration()
+        {
+            return PartialView("_Immigration");
+        }
+        public IActionResult AssetAndLicense()
+        {
+            return PartialView("_AssetAndLicense");
+        }
+        public IActionResult Reporting()
+        {
+            return PartialView("_Reporting ");
+        }
+        public IActionResult Experience()
+        {
+            return PartialView("_Experience ");
+        }
+        public IActionResult Qualification()
+        {
+            return PartialView("_Qualification ");
+        }
+        public IActionResult Skills()
+        {
+            return PartialView("_Skills ");
+        }
+        public IActionResult Attachments()
+        {
+            return PartialView("_Attachments ");
+        }
+        public IActionResult Allowance()
+        {
+            return PartialView("_Allowance ");
+        }
+        public IActionResult Salary()
+        {
+            return PartialView("_Salary");
+        }
     }
 }
