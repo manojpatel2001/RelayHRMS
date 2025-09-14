3d792f583e83d300211272b6550daefc8d012f98
Rita Girase <ritagirase9@gmial.com>
2025-09-14 23:15:51 +0530

    Todays Attendance

--- HRMS_Core/VM/Leave/TodaysAttendanceAdminViewModel.cs
+++ HRMS_Core/VM/Leave/TodaysAttendanceAdminViewModel.cs
@@ -16,6 +16,7 @@ namespace HRMS_Core.VM.Leave
         public string? ShiftName { get; set; }
         public string? DesignationName { get; set; }
         public string? BranchName { get; set; }
+        public string? EmployeeTypeName { get; set; }
         public string? ShiftTime { get; set; }
         public decimal? WorkingHours { get; set; }
         public decimal? SalaryDay { get; set; }
