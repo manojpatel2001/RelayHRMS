db1dcd367e7269b8e4c2eff684f37ae348d93e65
RameshDeveloper001 <chramesh7526@gmail.com>
2025-09-30 11:00:54 +0530

    Add controller Scheme

---
+++ HRMS.UI/Areas/AdminPanel/Controllers/SchemeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HRMS.UI.Areas.AdminPanel.Controllers
+{
+    [Area("AdminPanel")]
+    public class SchemeController : Controller
+    {
+        public IActionResult SchemeMaster()
+        {
+            return View();
+        }
+        public IActionResult SchemeDetails()
+        {
+            return View();
+        }
+    }
+}
