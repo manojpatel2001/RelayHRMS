a5c754eba10945cecd0cf11fc4b9e9abec7376ff
Rita Girase <ritagirase9@gmial.com>
2025-12-10 23:31:45 +0530

    CommanReason CURD

---
+++ HRMS_Infrastructure/Interface/CommanReason/ICommonReasonsRepository.cs
@@ -0,0 +1,16 @@
+﻿using HRMS_Core.VM.Leave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMS_Infrastructure.Interface.CommanReason
+{
+    public interface ICommonReasonsRepository:IRepository<LeaveCancellationReasonvm>
+    {
+
+        Task<List<LeaveCancellationReasonvm>> GetLeavecancellationReasons();
+
+    }
+}
