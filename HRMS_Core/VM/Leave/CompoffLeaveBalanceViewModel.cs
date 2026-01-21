7680b64d6b4035b1b5dc167f7ec2176086316648
Rita Girase <ritagirase9@gmial.com>
2026-01-21 17:56:50 +0530

    Comp_off validation

--- HRMS_Core/VM/Leave/CompoffLeaveBalanceViewModel.cs
+++ HRMS_Core/VM/Leave/CompoffLeaveBalanceViewModel.cs
@@ -8,6 +8,10 @@ namespace HRMS_Core.VM.Leave
 {
     public class CompoffLeaveBalanceViewModel
     {
-
+        public int TotalBalance { get; set; }
+        public int PendingLeaves { get; set; }
+        public int AvailableBalance { get; set; }
+        public DateTime LastBalanceDate { get; set; }
+        public string StatusMessage { get; set; }
     }
 }
